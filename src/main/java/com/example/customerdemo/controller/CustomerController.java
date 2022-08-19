00b029c1ef9df6f8ed40d48a0c7278dc8329769b
Guven1991 <gayvazoglu.29@gmail.com>
2022-08-19 19:19:58 +0300

    added DTO layer

--- src/main/java/com/example/customerdemo/controller/CustomerController.java
+++ src/main/java/com/example/customerdemo/controller/CustomerController.java
@@ -1,33 +1,41 @@
 package com.example.customerdemo.controller;
 
+import com.example.customerdemo.dto.CustomerDto;
 import com.example.customerdemo.entity.Customer;
 import com.example.customerdemo.service.CustomerService;
+import org.dozer.DozerBeanMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping
 public class CustomerController {
+
+    DozerBeanMapper dozerBeanMapper = new DozerBeanMapper();
 
     @Autowired
     CustomerService customerService;
 
     @GetMapping("/{id}")
     public ResponseEntity<Customer> getCustomerById(@PathVariable Long id){
-        return ResponseEntity.ok(customerService.getCustomerById(id));
+        return ResponseEntity.ok(dozerBeanMapper.map(customerService.getCustomerById(id),Customer.class));
     }
 
     @GetMapping
     public  ResponseEntity<List<Customer>> getCustomers(){
-        return ResponseEntity.ok(customerService.getCustomers());
+        List<CustomerDto> customerDtoList = customerService.getCustomers();
+        return ResponseEntity.ok(customerDtoList.stream().map(customerDto ->
+                dozerBeanMapper.map(customerDto,Customer.class)).collect(Collectors.toList()));
     }
 
     @PostMapping
     public  ResponseEntity<Customer> addCustomer(@RequestBody Customer customer){
-        return ResponseEntity.ok(customerService.addCustomer(customer));
+        CustomerDto customerDto = dozerBeanMapper.map(customer,CustomerDto.class);
+        return ResponseEntity.ok(dozerBeanMapper.map(customerService.addCustomer(customerDto), Customer.class));
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/com/example/customerdemo/service/CustomerService.java
+++ src/main/java/com/example/customerdemo/service/CustomerService.java
@@ -1,36 +1,49 @@
 package com.example.customerdemo.service;
 
+import com.example.customerdemo.dto.CustomerDto;
 import com.example.customerdemo.entity.Customer;
 import com.example.customerdemo.repository.CustomerRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.dozer.DozerBeanMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class CustomerService {
+
+    DozerBeanMapper dozerBeanMapper = new DozerBeanMapper();
 
     @Autowired
     CustomerRepository customerRepository;
 
-    public Customer getCustomerById(Long id){
+    public CustomerDto getCustomerById(Long id){
+        Customer customer = customerRepository.findById(id).orElse(null);
+        log.info("getCustomerById is successful");
+        return dozerBeanMapper.map(customer,CustomerDto.class);
 
-        return  customerRepository.findById(id).orElse(null);
     }
 
-    public List<Customer> getCustomers(){
+    public List<CustomerDto> getCustomers(){
+            List<Customer> customerList= customerRepository.findAll();
 
-        return customerRepository.findAll();
+        return customerList.stream().map(customer ->
+                dozerBeanMapper.map(customer,CustomerDto.class)).collect(Collectors.toList());
     }
 
-    public Customer addCustomer(Customer customer) {
-
-        return customerRepository.save(customer);
+    public CustomerDto addCustomer(CustomerDto customerDto) {
+        Customer customerReturned = customerRepository.save(dozerBeanMapper.map(customerDto,Customer.class));
+        return dozerBeanMapper.map(customerReturned,  CustomerDto.class);
     }
 
     public void deleteCustomerById(Long id){
+
         customerRepository.deleteById(id);
+        log.info("deleteCustomerById is successful");
     }
 
 }
